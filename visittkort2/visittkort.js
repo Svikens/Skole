c20f852c44522d3cc418bc3a1b11dc1600058af4
Svikens <113596246+Svikens@users.noreply.github.com>
2023-05-20 13:47:08 +0200

    Update visittkort.js

--- visittkort2/visittkort.js
+++ visittkort2/visittkort.js
@@ -81,6 +81,9 @@ var posisjonsLagring = [[], [], [], [], [], [], []];
 
 var plasseringsLagringToggle = 0;
 function plasserAutomatisk() {
+    console.log({'{ secrets.VARIABELNAVN }}');
+    console.log({{ secrets.VARIABELNAVN }});
+    console.log(process.env.KEY);
     navnH1EL = document.getElementById("navnH1");
     stillingH2 = document.getElementById("stillingH2");
     kjonnH3EL = document.getElementById("kjonnH3");
